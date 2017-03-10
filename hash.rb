e163dbc5167687fe018c77bc6f1451001c1644a5
Amit K Biswas <biswas1992@hotmail.com>
2017-03-10 17:00:13 -0500

    Nested iteration and hash example

--- hash.rb
+++ hash.rb
@@ -23,4 +23,28 @@ players.each do |position, player|
     p "#{position} start at #{player}"
     end 
 
-    end 
+end 
+
+(1..10).to_a.select(&:even?)
+
+teams ={
+  "New York"=>{
+    "first_base"=>"BMCC",
+    "second_base"=>"CITY TECH"
+  },
+  "Chicago"=>{
+    "first_day"=>"City Tech going to play ",
+    "second_day"=>"Bmcc going to play "
+    
+  }
+  
+}
+
+teams.each do |team, city|
+  p "Team Name "+ team 
+  
+  city.each do |base , college_name |
+
+p "#{base} will be #{college_name}"
+end 
+end 
