8836b040db1daf117c300ab69cc4c0e80150b79e
Amit K Biswas <biswas1992@hotmail.com>
2017-03-24 17:32:25 -0400

    each iterator

--- while.rb
+++ while.rb
@@ -1,17 +1,24 @@
 i = 0
 
-while i < 10 
+while i < 10
     puts " Hello Mazegeek"
 
-i += 1  
-end 
+i += 1
+end
 
 arr = [23,65,89,63,]
 
 arr.each do |i|
-    p i 
-end 
-
-for i in 0..10 
     p i
-    end 
+end
+
+for i in 0..10
+    p i
+    end
+
+
+    ## each iterator
+Array =[23, 456,901,862,'Amit']
+Array.each do |i|
+  p i
+end 
